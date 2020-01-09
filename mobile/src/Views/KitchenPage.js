17b1708ff73621f4f647625ed45a7412e6f65729
Emil Kolczyński <emil.kolczynskii@gmail.com>
2020-01-09 19:31:10 +0100

    [add] KitchenPage

---
+++ mobile/src/Views/KitchenPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import styled from 'styled-components'
+import { Component } from 'react'
+
+
+class KitchenPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+                <Text>TEST</Text>
+            </View>
+        );
+    }
+}
